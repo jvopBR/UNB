62cf6ba4d8147772b565e7503a02b64a7a99218d
AnotherOne <95287980+jvopBR@users.noreply.github.com>
2023-02-20 11:55:13 -0300

    Create GastoDeCombustivel.c

---
+++ APC.2022.2/Extras/GastoDeCombustivel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main () {
+        double h, km, r;
+            scanf("%lf", &h);
+            scanf("%lf", &km);
+
+            r = (h*km)/12;
+            
+            printf("%0.3f\n", r);
+
+
+    }
