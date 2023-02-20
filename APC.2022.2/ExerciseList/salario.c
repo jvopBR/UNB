05233da0eeb359fc77d2c4625d643d371aed2bd7
AnotherOne <95287980+jvopBR@users.noreply.github.com>
2023-02-20 11:46:36 -0300

    Create salario.c

---
+++ APC.2022.2/ExerciseList/salario.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    int main()
+        {
+            int f, h;
+            float g,s;
+            scanf("%d", &f);
+            scanf("%d", &h);
+            scanf("%f", &g);
+
+                s = h * g;
+
+            printf("NUMBER = %d\n", f);
+            printf("SALARY = U$ %0.2f\n", s);
+        }
