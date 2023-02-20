8d5a21a37bb36e908e4fd6578dc57e57cc407da0
AnotherOne <95287980+jvopBR@users.noreply.github.com>
2023-02-20 11:50:27 -0300

    Create Consumo.c

---
+++ APC.2022.2/Extras/Consumo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main () {
+        float d, c, r;
+            scanf("%f", &d);
+            scanf("%f", &c);
+
+            r = d / c;
+            printf("%0.3f km/l\n", r);
+    }
