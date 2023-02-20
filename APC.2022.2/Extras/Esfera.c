9fd2831d63e33388a48662254e4318988f5da1d8
AnotherOne <95287980+jvopBR@users.noreply.github.com>
2023-02-20 11:53:43 -0300

    Create Esfera.c

---
+++ APC.2022.2/Extras/Esfera.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+    int main () {
+        int r;
+        scanf("%i", &r);
+        printf("VOLUME = %0.3f\n", (4.0 * 3.14159 * pow(r,3)/3));            
+    }
